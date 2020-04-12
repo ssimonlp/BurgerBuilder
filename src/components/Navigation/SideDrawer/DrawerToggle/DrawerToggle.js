69c0fd3fd686313cc6d3c9dca7dfa8f768215d87
ssimonlp <simon.lepavec@gmail.com>
2020-04-12 11:39:19 +0200

    added burger icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import classes from './DrawerToggle.css'
 
 const drawerToggle = props => (
     <div
+        className={classes.DrawerToggle}
         onClick={props.click}
     >
-        MENU
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 );
 
